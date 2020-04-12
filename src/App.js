539efbe78663cdf4871d35ba2e30ed28520734a3
Brian Andrews <sbardian@gmail.com>
2020-04-12 05:59:39 -0600

    refactor: update app to use updated component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React from 'react'
+import MoonLoader from 'react-spinners/MoonLoader'
+import { css } from '@emotion/core'
 import './App.css'
 import WotQuotes from './components/wot-quotes'
 
+//  `https://sbardian.api.stdlib.com/wotQuotes@0.0.1/?credit=Thom&limit=3`
+//  `https://sbardian.api.stdlib.com/wotQuotes@0.0.1/?credit=Thom`
+//  `https://sbardian.api.stdlib.com/wotQuotes@0.0.1/?limit=5`
+//  `https://sbardian.api.stdlib.com/wotQuotes@0.0.1/?random=true`
+
 function App() {
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `
+
   return (
     <div className="App">
       <header className="App-header">
-        <WotQuotes random={true}>
-          {({ state, data }) => {
+        <WotQuotes
+          url={`https://sbardian.api.stdlib.com/wotQuotes@0.0.1/?random=true`}
+        >
+          {({ state, resetFetch }) => {
             if (state.value === 'idle') return <div>idle...</div>
-            if (state.value === 'loading') return <div>fetching data!</div>
-            if (state.value === 'failure') return <div>failure. . . :( </div>
+            if (state.value === 'loading')
+              return (
+                <div>
+                  {' '}
+                  <MoonLoader css={override} size={150} color={'#123abc'} />
+                </div>
+              )
+            if (state.value === 'failure')
+              return (
+                <div>
+                  failure. . . :({' '}
+                  <button typ="button" onClick={() => resetFetch()}>
+                    Reset
+                  </button>{' '}
+                </div>
+              )
             if (
               state.value === 'success' &&
-              data &&
-              data.quotes &&
-              data.quotes.length > 0
+              state?.context?.data?.quotes?.length > 0
             ) {
+              const {
+                context: { data },
+              } = state
               return (
-                <div style={{ maxWidth: '800px' }}>
+                <div style={{ maxWidth: '800px', margin: '20px' }}>
                   {data.quotes.map(quote => {
                     return (
                       <div
+                        key={quote.id}
                         style={{
                           display: 'grid',
                           gridTemplateRows: '1fr 1fr',
@@ -41,9 +72,16 @@ function App() {
                 </div>
               )
             }
-            if (state.value === 'success' && data && data.quote) {
+            if (
+              state.value === 'success' &&
+              state.context.data &&
+              state.context.data.quote
+            ) {
+              const {
+                context: { data },
+              } = state
               return (
-                <div style={{ maxWidth: '800px' }}>
+                <div style={{ maxWidth: '800px', margin: '20px' }}>
                   <div
                     style={{
                       display: 'grid',
